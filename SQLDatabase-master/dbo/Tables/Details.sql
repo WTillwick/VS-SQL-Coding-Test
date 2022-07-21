6d0d50014ad23e0baf00a9805fc3b9f71f57f78a
WTillwick <108569125+WTillwick@users.noreply.github.com>
2022-07-21 17:23:59 +0200

    Add files via upload

---
+++ SQLDatabase-master/dbo/Tables/Details.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Details]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ItemDescription] NVARCHAR(50) NOT NULL, 
+    [Quantity] INT NOT NULL, 
+    [UnitPrice] MONEY NOT NULL, 
+    [LineTotal] MONEY NOT NULL, 
+    [HeaderInvoiceNumber] NVARCHAR(50) NOT NULL, 
+    CONSTRAINT [FK_Details_Header] FOREIGN KEY ([HeaderInvoiceNumber]) REFERENCES [Header]([InvoiceNumber])
+)
---
+++ SQLDatabase-master/dbo/Tables/Header.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Header]
+(
+	[InvoiceNumber] NVARCHAR(50) NOT NULL PRIMARY KEY, 
+    [InvoiceDate] NVARCHAR(50) NOT NULL, 
+    [CustomerName] NVARCHAR(50) NOT NULL, 
+    [CustomerAddress] NVARCHAR(50) NULL, 
+    [CustomerTelNumber] NVARCHAR(50) NOT NULL
+)
